a255b8eeb903d2aece147c12ac1707bc9d19c3ed
manato-takahashi <manato.takahashi86@gmail.com>
2024-02-09 00:46:58 +0900

    import: Ultimate Animated Characterをインポート

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    const string IDLE = "Idle";
+    const string WALK = "Walk";
+
+    CustomActions input;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    [Header("Movement")]
+    [SerializeField] ParticleSystem clickEffect;
+    [SerializeField] LayerMask clickableLayers;
+
+    float lookRotationSpeed = 8f;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    void AssignInputs()
+    {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    void ClickToMove()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
+        {
+            agent.destination = hit.point;
+            if(clickEffect != null)
+            {
+                Instantiate(clickEffect, hit.point += new Vector3(0, 0.1f, 0), clickEffect.transform.rotation);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        input.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.Disable();
+    }
+
+    void Update()
+    {
+        if(agent.velocity.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+}

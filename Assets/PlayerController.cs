c855a107a53cbf6250cefc7d2507c58c71da8542
manato-takahashi <manato.takahashi86@gmail.com>
2024-02-09 02:14:12 +0900

    add: click to move機能を実装

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -19,11 +19,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] LayerMask clickableLayers;
 
     float lookRotationSpeed = 8f;
-
-    void Awake()
+    
+    void Awake() 
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+
+        // Stopping distanceを適切な値に設定
+        agent.stoppingDistance = 0.5f;
 
         input = new CustomActions();
         AssignInputs();
@@ -37,36 +40,55 @@ public class PlayerController : MonoBehaviour
     void ClickToMove()
     {
         RaycastHit hit;
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers)) 
         {
             agent.destination = hit.point;
             if(clickEffect != null)
+            { Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+        }
+    }
+
+    void OnEnable() 
+    { input.Enable(); }
+
+    void OnDisable() 
+    { input.Disable();}
+
+    void FaceTarget()
+    {
+        // 移動している（エージェントの速度が0ではない）時のみ回転するようにします。
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            Vector3 direction = (agent.destination - transform.position).normalized;
+            // Y軸の回転は無視するために、Y成分を0に設定します。
+            direction.y = 0;
+
+            // directionが非常に小さいベクトル（ほぼゼロ）の場合は回転をスキップします。
+            if (direction.magnitude > 0.1f)
             {
-                Instantiate(clickEffect, hit.point += new Vector3(0, 0.1f, 0), clickEffect.transform.rotation);
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
             }
         }
     }
 
-    void OnEnable()
-    {
-        input.Enable();
-    }
 
-    void OnDisable()
+    void Update() 
     {
-        input.Disable();
-    }
+        // エージェントが目的地に到達したかどうかの判定を追加
+        bool hasReachedDestination = !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance;
 
-    void Update()
-    {
-        if(agent.velocity.magnitude > 0)
+        // 移動しているかどうかで回転を制御
+        if (!hasReachedDestination)
         {
-            animator.SetBool("isWalking", true);
+            FaceTarget();
+            animator.SetBool("IsMoving", true);
         }
         else
         {
-            animator.SetBool("isWalking", false);
+            animator.SetBool("IsMoving", false);
         }
     }
+
 
 }
